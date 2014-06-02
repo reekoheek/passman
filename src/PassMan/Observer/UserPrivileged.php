38c5f44cdec3587fcf37700a962c6339a891cda4
Ganesha <reekoheek@gmail.com>
2014-06-02 18:46:57 +0700

    added privileges page

--- src/PassMan/Observer/UserPrivileged.php
+++ src/PassMan/Observer/UserPrivileged.php
@@ -8,4 +8,14 @@ class UserPrivileged
     {
         $collection->criteria['$privileges'] = @$_SESSION['user']['username'];
     }
+
+    public function saving($model)
+    {
+        if ($model->isNew() && isset($_SESSION['user']['username'])) {
+            $model['$privileges'] = array(
+                $_SESSION['user']['username'],
+            );
+        }
+
+    }
 }
